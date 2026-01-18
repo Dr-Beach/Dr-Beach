00ff1bc2a01e01a02803100e88c3d0aa341d72d9
Stefan <test@Mac.fritz.box>
2026-01-18 17:53:58 +0100

    Use standard node imports

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,5 @@
-import fs from "node:fs";
-import path from "node:path";
+import fs from "fs";
+import path from "path";
 import { content } from "./content";
 
 const outputDir = path.resolve(process.cwd(), "public");
